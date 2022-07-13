6fcbd391fcebac5dc95e3a5f6f9e16d0a45ac38a
Gunel <gunelfh@code.edu.az>
2022-07-13 14:46:22 +0400

    First Commit

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProniaApp.DAL
+{
+	public class ApplicationDbContext:DbContext
+	{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+	}
+}
+
